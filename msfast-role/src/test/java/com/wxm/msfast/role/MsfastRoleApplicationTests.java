39b13b0e002ca2e615f35169feb0e9f2a2d4d9a2
sc_wanglei <378526425@qq.com>
2022-04-29 13:48:05 +0800

    角色权限初始化

--- msfast-role/src/test/java/com/wxm/msfast/role/MsfastRoleApplicationTests.java
+++ msfast-role/src/test/java/com/wxm/msfast/role/MsfastRoleApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wxm.msfast.role;
 
+import com.wxm.msfast.role.entity.UserEntity;
+import com.wxm.msfast.role.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MsfastRoleApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
     @Test
     void contextLoads() {
+        List<UserEntity> entityList = userService.list();
+        System.out.println(entityList);
     }
 
 }
