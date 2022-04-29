6852cd1cc77e0930818b103bbaf959897c4f6ef3
sc_wanglei <378526425@qq.com>
2022-04-29 17:40:31 +0800

    增加测试服务

---
+++ msfast-test/src/test/java/com/wxm/msfast/test/MsfastTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxm.msfast.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsfastTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
