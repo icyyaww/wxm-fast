2dbca85616ae6e2ae75755038df0000253c5a70d
sc_wanglei <378526425@qq.com>
2022-04-29 11:31:41 +0800

    角色权限初始化

---
+++ msfast-role/src/main/java/com/wxm/msfast/role/MsfastRoleApplication.java
@@ -0,0 +1,13 @@
+package com.wxm.msfast.role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsfastRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsfastRoleApplication.class, args);
+	}
+
+}
---
+++ msfast-role/src/test/java/com/wxm/msfast/role/MsfastRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxm.msfast.role;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsfastRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
