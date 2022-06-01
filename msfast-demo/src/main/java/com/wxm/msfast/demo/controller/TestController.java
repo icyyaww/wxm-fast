fb222457122c9b2c3eac0a3d1f1a94f27dfd7e63
sc_wanglei <378526425@qq.com>
2022-06-01 18:34:00 +0800

    增加配置文件

--- msfast-demo/src/main/java/com/wxm/msfast/demo/controller/TestController.java
+++ msfast-demo/src/main/java/com/wxm/msfast/demo/controller/TestController.java
@@ -3,7 +3,12 @@ package com.wxm.msfast.demo.controller;
 import com.wxm.msfast.base.common.utils.R;
 import com.wxm.msfast.demo.feign.RoleFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @program: msfast
@@ -11,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
  * @author: Mr.Wang
  * @create: 2022-04-29 17:48
  **/
+@RefreshScope
 @RestController
 @RequestMapping("demo")
 public class TestController {
@@ -18,11 +24,36 @@ public class TestController {
     @Autowired
     RoleFeignService roleFeignService;
 
+    @Value("${ms.user.name}")
+    String userName;
+
+    @Value("${ms.user.age}")
+    int age;
+
     /**
-     * 角色详情
+     * @Description: 测试openFeign 远程调用角色权限服务
+     * @Param:
+     * @return:
+     * @Author: Mr.Wang
+     * @Date: 2022/5/31 下午3:47
      */
     @GetMapping("/role/{id}")
     public R roleInfo(@PathVariable(name = "id") Long id) {
         return roleFeignService.info(id);
+    }
+
+    /**
+     * @Description: 测试读取nacos配置中心 远程配置
+     * @Param:
+     * @return:
+     * @Author: Mr.Wang
+     * @Date: 2022/5/31 下午3:47
+     */
+    @GetMapping("/config")
+    public R nacosConfig() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("userName", userName);
+        result.put("age", age);
+        return R.ok(result);
     }
 }
