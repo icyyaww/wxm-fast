a6c9c7d0200b2d685b5011a42b6830e14dfce0ec
sc_wanglei <378526425@qq.com>
2022-06-28 16:17:06 +0800

    增加监控

--- msfast-modules/msfast-demo/src/main/java/com/wxm/msfast/demo/controller/UserController.java
+++ msfast-modules/msfast-demo/src/main/java/com/wxm/msfast/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.wxm.msfast.base.auth.annotation.AuthIgnore;
 import com.wxm.msfast.base.common.web.domain.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +33,7 @@ public class UserController {
      * 列表
      */
     @RequestMapping("/list")
+    @AuthIgnore
     //@RequiresPermissions("demo:user:list")
     public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = userService.queryPage(params);
