33fb9acbc03d35eb7236c82a329df279031591ff
sc_wanglei <378526425@qq.com>
2022-10-10 14:02:41 +0800

    修改限制

--- msfast-modules/msfast-community/src/main/java/com/wxm/msfast/community/controller/FrUserController.java
+++ msfast-modules/msfast-community/src/main/java/com/wxm/msfast/community/controller/FrUserController.java
@@ -10,10 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiOperationSort;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -41,8 +38,8 @@ public class FrUserController {
     }
 
     @ApiOperation("首页动态用户")
-    @ApiOperationSort(value = 1)
-    @PostMapping("/dynamic/user")
+    @ApiOperationSort(value =1)
+    @GetMapping("/dynamic/user")
     @AuthIgnore
     public R<List<DynamicUserResponse>> dynamicUser() {
         return R.ok(frUserService.getDynamicUser());
