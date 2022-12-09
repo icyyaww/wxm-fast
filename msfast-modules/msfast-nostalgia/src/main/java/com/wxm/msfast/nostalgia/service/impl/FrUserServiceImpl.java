dc45cea9c779cb323df4e5d168f4285c444dde3c
sc_wanglei <378526425@qq.com>
2022-12-09 18:19:45 +0800

    首页推荐用户信息

--- msfast-modules/msfast-nostalgia/src/main/java/com/wxm/msfast/nostalgia/service/impl/FrUserServiceImpl.java
+++ msfast-modules/msfast-nostalgia/src/main/java/com/wxm/msfast/nostalgia/service/impl/FrUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wxm.msfast.nostalgia.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.wxm.msfast.base.auth.entity.LoginUser;
@@ -46,11 +47,15 @@ public class FrUserServiceImpl extends ServiceImpl<FrUserDao, FrUserEntity> impl
                 throw new JrsfException(UserExceptionEnum.SEARCH_PARAM_EMPTY_EXCEPTION);
             }
             Map<String, Object> param = new HashMap<>();
-            param.put("id", 1);
+            param.put("gender", request.getGender().name());
+            //todo 增加年龄筛选
             List<RecommendUserInfoResponse> list = this.baseMapper.getRecommendUserInfo(param);
-            RecommendUserInfoResponse recommendUserInfo = new RecommendUserInfoResponse();
-            BeanUtils.copyProperties(list.get(0), recommendUserInfo);
-            return recommendUserInfo;
+            if (CollectionUtil.isNotEmpty(list)) {
+                RecommendUserInfoResponse recommendUserInfo = new RecommendUserInfoResponse();
+                BeanUtils.copyProperties(list.get(0), recommendUserInfo);
+                return recommendUserInfo;
+            }
+
         } else {
             //已登录
         }
