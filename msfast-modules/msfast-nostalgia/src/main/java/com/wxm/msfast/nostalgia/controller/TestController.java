b6e468828b9d182809c00e10a3a734d24235b841
sc_wanglei <378526425@qq.com>
2022-12-12 17:44:43 +0800

    匹配

--- msfast-modules/msfast-nostalgia/src/main/java/com/wxm/msfast/nostalgia/controller/TestController.java
+++ msfast-modules/msfast-nostalgia/src/main/java/com/wxm/msfast/nostalgia/controller/TestController.java
@@ -12,7 +12,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiOperationSort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
