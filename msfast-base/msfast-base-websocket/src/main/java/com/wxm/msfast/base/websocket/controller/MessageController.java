36fb15ecbd9600ed1647f9d11e59cda9dcab1cec
sc_wanglei <378526425@qq.com>
2023-02-07 16:59:36 +0800

    消息列表

--- msfast-base/msfast-base-websocket/src/main/java/com/wxm/msfast/base/websocket/controller/MessageController.java
+++ msfast-base/msfast-base-websocket/src/main/java/com/wxm/msfast/base/websocket/controller/MessageController.java
@@ -2,18 +2,14 @@ package com.wxm.msfast.base.websocket.controller;
 
 import com.wxm.msfast.base.common.constant.ParamTypeConstants;
 import com.wxm.msfast.base.common.utils.PageResult;
-import com.wxm.msfast.base.common.utils.TokenUtils;
 import com.wxm.msfast.base.common.web.domain.R;
 import com.wxm.msfast.base.websocket.common.rest.response.MessageInfoResponse;
 import com.wxm.msfast.base.websocket.common.rest.response.MessageListResponse;
 import com.wxm.msfast.base.websocket.service.MsFastMessageService;
-import com.wxm.msfast.base.websocket.utils.ChannelUtil;
 import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
 
 /**
  * @program: wxm-fast
